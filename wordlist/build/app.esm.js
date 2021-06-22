69a5b96e842af6b362abe033acef19b8ca56fb8d
Philip Heltweg <pheltweg@gmail.com>
2021-06-22 20:17:04 +0200

    Updates

--- wordlist/build/app.esm.js
+++
@@ -1,1 +0,0 @@
-import{p as o,b as r}from"./p-ccd19291.js";(()=>{const r=import.meta.url,s={};return""!==r&&(s.resourcesUrl=new URL(".",r).href),o(s)})().then((o=>r([["p-ebb60c6c",[[1,"app-root"],[1,"word-list",{name:[1],words:[16],maxWordsShown:[2,"max-words-shown"],availableWords:[32]}]]]],o)));
