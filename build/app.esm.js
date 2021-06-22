6e6ccba72c785c88d4c0177c5a619368ee3e3c05
Philip Heltweg <pheltweg@gmail.com>
2021-06-22 19:28:59 +0200

    Updates

--- build/app.esm.js
+++ build/app.esm.js
@@ -1,1 +1,1 @@
-import{p as o,b as r}from"./p-31589c69.js";(()=>{const r=import.meta.url,s={};return""!==r&&(s.resourcesUrl=new URL(".",r).href),o(s)})().then((o=>r([["p-47208650",[[1,"app-root"],[1,"word-list",{name:[1],words:[16],maxWordsShown:[2,"max-words-shown"],availableWords:[32]}]]]],o)));
+import{p as o,b as r}from"./p-ccd19291.js";(()=>{const r=import.meta.url,s={};return""!==r&&(s.resourcesUrl=new URL(".",r).href),o(s)})().then((o=>r([["p-ebb60c6c",[[1,"app-root"],[1,"word-list",{name:[1],words:[16],maxWordsShown:[2,"max-words-shown"],availableWords:[32]}]]]],o)));
